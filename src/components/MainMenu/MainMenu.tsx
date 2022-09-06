7867d3f45922de62a5d2dfd2dec8d6679886683f
Quentin Gibson <quentingibson94@gmail.com>
2022-09-06 12:39:37 -0400

    create main menu

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,11 +1,23 @@
 import MenuButton from "../MenuButton/MenuButton";
+import Logo from "../../assets/vision-trainer-logo.png";
 
 const MainMenu = () => {
   return (
-    <div className="grid gap-4 grid-cols-2 justify-items-center max-w-sm mx-auto">
-      <MenuButton to="/game">New Game</MenuButton>
-      <MenuButton to="/leaderboard">LeaderBoard</MenuButton>
-    </div>
+    <>
+      <div className="grid gap-4 grid-rows-2 max-w-md mx-auto my-7 justify-items-center">
+        <div className="">
+          <img
+            src={Logo}
+            alt="Logo for Chess Vision Trainer"
+            className="object-cover"
+          />
+        </div>
+        <div className="grid gap-4 grid-cols-2 justify-items-center self-center">
+          <MenuButton to="/game">New Game</MenuButton>
+          <MenuButton to="/leaderboard">LeaderBoard</MenuButton>
+        </div>
+      </div>
+    </>
   );
 };
 
